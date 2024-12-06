ba0aaffc1ac6dffbdc331c664a0457329d6c7074
Badinage11 <147810289+Badinage11@users.noreply.github.com>
2024-12-06 18:55:41 +0800

    Updated the code for the Employee section

--- src/Employee.java
+++ src/Employee.java
@@ -3,16 +3,15 @@ public class Employee extends Person {
     private String position;
 
     public Employee() {
-        // Default constructor
+        super();
     }
 
-    public Employee(String name, int age, String contactNumber, String employeeId, String position) {
-        super(name, age, contactNumber);
+    public Employee(String name, int age, String gender, String employeeId, String position) {
+        super(name, age, gender);
         this.employeeId = employeeId;
         this.position = position;
     }
 
-    // Getters and Setters
     public String getEmployeeId() {
         return employeeId;
     }
